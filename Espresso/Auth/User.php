610e3a0408a6da0263f776e636332c0f90a2e9ba
jammincoder <jammincoderguy96@gmail.com>
2022-04-06 21:15:27 -0400

    Commented code

--- Espresso/Auth/User.php
+++ Espresso/Auth/User.php
@@ -9,16 +9,19 @@ use Espresso\Auth\SessionVars;
 
 class User
 {   
-
+    // Any errors that arize are stored here
     public static string $error = "";
 
     public static function genUID($length = 32)
     {
+        // Generates a random string ID.
         return substr(str_replace(['+', '/', '='], '', base64_encode(random_bytes(32))), 0, $length);
     }
 
     public static function emailValid($email)
     {
+        // Verifies that the $email is in the 'email@email.com' format, 
+        // and that its length is less than 100.
         if (
             strlen($email) < 100 && 
             filter_var($email, FILTER_VALIDATE_EMAIL)
@@ -29,6 +32,8 @@ class User
     }
 
     public static function usernameValid($username) {
+        // Verifies that the $username contains only letters from A to Z, numbers, and underscores
+        // AND that the length is less than 100.
         if (
             preg_match("/^[a-zA-Z0-9_]+$/", $username) && 
             strlen($username) < 100
@@ -41,12 +46,14 @@ class User
 
     public static function passwordValid($password)
     {
+        // Verifies that the $password length is greater than 8.
         if (strlen($password) >= 8) return true;
         return false;
     }
 
     public static function passwordMatchesUserInfo($password, $userRow)
     {
+        // Verifies that the provided $password matches the password in the user's data row.
         if (password_verify($password, $userRow["hashed_password"])) {
             return true;
         }
@@ -56,6 +63,7 @@ class User
 
     private static function getUserRow($identifier)
     {
+        // Gets the user's data row from the DB based on the $identifier.
         return DB::query(
             "SELECT * FROM users WHERE email = (:identifier) OR username = (:identifier) OR uuid = (:identifier)",
             ["identifier" => $identifier]
@@ -65,14 +73,26 @@ class User
 
     public static function login($identifier, $password)
     {
+        /**
+         * Log user in with provided $identifier (username or email) and password,
+         * then start a new session with the $_SESSION['username']
+         */
+
+
+        // Get the user information from DB, if any exists. If it doesn't exist
+        // then there is no user with this $indentifier (username or password)
         $userRow = self::getUserRow($identifier);
 
+        // Verify login info
         if (!$userRow || !self::passwordMatchesUserInfo($password, $userRow)) {
-            // User doesn't exist
+            // If true, either user does not exist, OR the password or username was wrong,
+            // so set the error and return.
             self::$error = "Those credentials don't match our records.";
             return;
         }
 
+
+        // Login was successful, create a new session for the user.
         session_start();
         session_regenerate_id(true);
         $_SESSION[SessionVars::UUID] = $userRow["uuid"];
