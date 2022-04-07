58006aec9c844043622ebba426a7ea6674a53fe2
jammincoder <jammincoderguy96@gmail.com>
2022-04-06 20:40:08 -0400

    Commented code.

--- Espresso/Auth/XCSRF.php
+++ Espresso/Auth/XCSRF.php
@@ -4,27 +4,25 @@ namespace Espresso\Auth;
 
 class XCSRF
 {
+    // Constant name value to access 'xcsrf_token' in the $_SESSION
     const XCSRF_TOKEN = "xcsrf_token";
-
-    public static function newToken() 
-    {
-        return bin2hex(random_bytes(32));
-    }
 
     public static function setNewCSRFToken()
     {
-        $_SESSION[self::XCSRF_TOKEN] = self::newToken();
+        // Sets new random anti CSRF token. 
+        $_SESSION[self::XCSRF_TOKEN] = bin2hex(random_bytes(32));
     }
 
     public static function csrfInput()
     {
+        // Returns <input type='hidded'> with value set to the new random anti CSRF token.
         self::setNewCSRFToken();
         return "<input type='hidden' name='" . self::XCSRF_TOKEN . "' value='" . $_SESSION[self::XCSRF_TOKEN] . "'>";
     }
 
     public static function tokenIsOK($postedToken)
     {
-        session_start();
+        // Checks if submitted XCSRF token equals the current XCSRF token stored in the $_SESSION
         if ($_SESSION[self::XCSRF_TOKEN] === $postedToken){
             return true;
         }
